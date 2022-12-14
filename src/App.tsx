d99ed5c9f1201c709ea25ca7c38bee8660cb867f
Nikola Lukaszewicz <nicola.lukaszewicz1@gmail.com>
2022-12-14 17:15:03 +0100

    finish adding todo

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function App() {
   const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("")
   return (
     <div style={{ margin: "50px" }}>
       <input
@@ -9,6 +10,12 @@ function App() {
         onChange={(e) => setLogin(e.target.value)}
       />
       <p>Login: {login}</p>
+
+      <input
+      placeholder="Please write your password..."
+      onChange={(e) => setPassword(e.target.value)}
+      />
+      <p>Password: {password}</p>
     </div>
   );
 }
