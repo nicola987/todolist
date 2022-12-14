8e7271da5bd4a3d3f8edd01777037834dfbc2b35
Wojciech Michalowski <wojciech.michalowski1@wp.pl>
2022-12-14 16:53:27 +0100

    add login field

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function App() {
+  const [login, setLogin] = useState("");
   return (
-    <div>
-      test
+    <div style={{ margin: "50px" }}>
+      <input
+        placeholder="Please write your login..."
+        onChange={(e) => setLogin(e.target.value)}
+      />
+      <p>Login: {login}</p>
     </div>
   );
 }
