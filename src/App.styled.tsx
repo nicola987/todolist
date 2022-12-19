d45a8b68ca908f66d576d1fa3f2b0c4c47912999
nicola987 <120508490+nicola987@users.noreply.github.com>
2022-12-19 21:59:11 +0100

    finish adding styled (#4)

---
+++ src/App.styled.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const StyledInput = styled.input`
+border: 2px solid red;
+border-radius:5px;
+`
+
+export const StyledField = styled.div`
+ color:blue;
+`
+
+export const StyledPassInput = styled.input`
+border: 2px solid green;
+border-radius:5px;
+`
+
+export const StyledPassField = styled.div`it
+color:green;
+`
+
+export const StyledNameInput = styled.input`
+border: 2px solid violet;
+border-radius:5px;
+`
+
+export const StyledNameField = styled.div`
+color: orange;
+`
